8d0cb86b0edff24ffcd212f7fa21fb72635751f3
vamalyi <v.a.malyi@gmail.com>
2017-01-21 18:08:39 +0200

    first views and urls

--- settings/urls.py
+++ settings/urls.py
@@ -1,21 +1,17 @@
-"""skymarket URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+from webmarket.views import (IndexView, ClientProfileView, PilotProfileView, PilotsCatalogueView,
+                            OrderView, OrdersCatalogueView)
+
 urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', IndexView.as_view(), name='index'),
+
+    url(r'^categories/$', CategoryEditListView.as_view(), name='category-list'),
+    url(r'^categories/create/$', CategoryCreateView.as_view(), name='category-create'),
+    url(r'^categories/(?P<pk>\d+)/$', CategoryDetailEditListView.as_view(), name='category-detail'),
+    url(r'^categories/(?P<pk>\d+)/update/$', CategoryUpdateView.as_view(), name='category-update'),
 ]
--- webmarket/views.py
+++ webmarket/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.views.generic import TemplateView, DetailView, ListView
 
-# Create your views here.
+from .models import *
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class ClientProfileView(DetailView):
+    template_name = 'webmarket/client_profile.html'
+    context_object_name = 'client'
+
+    def get_object(self, queryset=None):
+        obj = ClientProfile.objects.first()
+        return obj
+
+
+class PilotProfileView(DetailView):
+    template_name = 'webmarket/pilot_profile.html'
+    context_object_name = 'pilot'
+
+    def get_object(self, queryset=None):
+        obj = PilotProfile.objects.first()
+        return obj
+
+
+class PilotsCatalogueView(ListView):
+    template_name = 'webmarket/pilots_catalogue.html'
+    context_object_name = 'pilots'
+
+    def get_queryset(self):
+        qs = PilotProfile.objects.all()
+        return qs
+
+
+class OrderView(DetailView):
+    template_name = 'webmarket/order_detail.html'
+    context_object_name = 'order'
+
+    def get_object(self, queryset=None):
+        obj = Order.objects.first()
+        return obj
+
+
+class OrdersCatalogueView(ListView):
+    template_name = 'webmarket/orders_catalogue.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        qs = Order.objects.all()
+        return qs
+
+
+
+
+
